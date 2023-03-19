65cbf32584bab7413d6be09a0ac36ec8dc8f9cbb
Shashank <sha.dev.co.in@gmail.com>
2023-03-19 18:29:05 +0530

    2023Mar19: Learn't GPIO Interrupt driver and adding sphinx docs
    - Added code for GPIO Interrupt driver.
    - .clang.code-workspace is updated since gpio.h header is added
      in source code.
    - Added sphinx documentation for 36 gpio irq driver.
    
    Signed-off-by: Shashank <sha.dev.co.in@gmail.com>

--- kcodes/36_gpio_interrupt/gpioirq.c
+++ kcodes/36_gpio_interrupt/gpioirq.c
@@ -8,17 +8,35 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/err.h>
-#include <linux/err.h>
+#include <linux/delay.h>
+#include <linux/interrupt.h>
 #include <linux/gpio.h>
+#include <asm-generic/gpio.h>
+/*
+** EN_DEBOUNCE - Raspberry pi doesn't support DEBOUNCE so in case
+** this macro is enabled, we will have a software hack to stop
+** spurious interrupts from triggering.
+*/
+#define EN_DEBOUNCE
+#ifdef EN_DEBOUNCE
+#include <linux/jiffies.h>
+unsigned long volatile jiffies = 0;
+unsigned long old_jiffie;
+#endif
 
-#define EXIT_SUCCESS 0
-#define FIRST_MINOR 0
-#define EXIT_FAILURE -1
-#define REQ_DEV_CNT 1
-#define DEV "sha"
-#define CLS_NAME_DEV "cls_sha"
-#define DEV_NAME_DEV "dev_sha"
-#define GPIO_21     (21)
+#define EXIT_SUCCESS    0
+#define FIRST_MINOR     0
+#define EXIT_FAILURE    -1
+#define REQ_DEV_CNT     1
+#define DEV             "sha"
+#define CLS_NAME_DEV    "cls_sha"
+#define DEV_NAME_DEV    "dev_sha"
+#define GPIO_21_OUT      21
+#define GPIO_25_IN       25
+
+unsigned int led_toggle = 0;
+unsigned int GPIO_irqNum;
+
 
 dev_t dev_num = 0;
 static struct class *cls_sha;
@@ -41,6 +59,23 @@ static struct file_operations mfops =
     .release = mrelease,
 };
 
+static irqreturn_t gpio_irq_handler(int irq, void *devid)
+{
+    static unsigned long flags = 0;
+#ifdef EN_DEBOUNCE
+    unsigned long diff = jiffies - old_jiffie;
+    if (diff < 20)
+        return IRQ_HANDLED;
+    old_jiffie = jiffies;
+#endif
+    local_irq_save(flags);
+    led_toggle = ( 0x01 ^ led_toggle);
+    gpio_set_value(GPIO_21_OUT, led_toggle);
+    pr_info("IRQ occured GPIO OUT %d\n", GPIO_21_OUT);
+    local_irq_restore(flags);
+    return IRQ_HANDLED;
+}
+
 static int mopen(struct inode *inode, struct file *file)
 {
     pr_info("%s: opened file\n", __func__);
@@ -57,7 +92,7 @@ static ssize_t mread(struct file *file, char __user *buf, size_t len, loff_t *of
 {
     uint8_t m_gpio_state;
 
-    m_gpio_state = gpio_get_value(GPIO_21);
+    m_gpio_state = gpio_get_value(GPIO_21_OUT);
     len = 1;
 
     if (copy_to_user(buf, &m_gpio_state, len) > 1) {
@@ -79,10 +114,10 @@ static ssize_t mwrite(struct file *file, const char __user *buf, size_t len, lof
 
     if (m_kbuf[0] == 1) {
         // Set the GPIO pin value to 1
-        gpio_set_value(GPIO_21, 1);
+        gpio_set_value(GPIO_21_OUT, 1);
     } else if (m_kbuf[0] == 0) {
         // Set the GPIO pin value to 0
-        gpio_set_value(GPIO_21, 0);
+        gpio_set_value(GPIO_21_OUT, 0);
     } else {
         pr_err("U can only set value to either 0 or 1\n");
     }
@@ -122,30 +157,68 @@ static int __init dinit(void)
     }
     pr_info("device is created successfully\n");
 
-    if (gpio_is_valid(GPIO_21) == false) {
-        pr_err("GPIO_21 is not valid\n");
-        goto cleanup_gpioisvalid;
+    if (gpio_is_valid(GPIO_21_OUT) == false) {
+        pr_err("GPIO OUT %d is not valid\n", GPIO_21_OUT);
+        goto cleanup_validgpio21;
     }
-    pr_info("GPIO_21 is valid\n");
+    pr_info("GPIO OUT %d is valid\n", GPIO_21_OUT);
 
-    if (gpio_request(GPIO_21, "GPIO_21") < 0) {
-        pr_err("GPIO_21 request failed\n");
-        goto cleanup_gpiorequest;
+    if (gpio_request(GPIO_21_OUT, "GPIO_21") < 0) {
+        pr_err("GPIO OUT %d request failed\n", GPIO_21_OUT);
+        goto cleanup_reqgpio21;
     }
-    pr_info("GPIO_21 request successful\n");
+    pr_info("GPIO OUT %d requested successfully\n", GPIO_21_OUT);
 
-    gpio_direction_output(GPIO_21, 0);
-    pr_info("GPIO_21 direction is set to output mode\n");
+    gpio_direction_output(GPIO_21_OUT, 0);
 
-    gpio_export(GPIO_21, false);
-    pr_info("GPIO_21 export is successful\n");
+    if (gpio_is_valid(GPIO_25_IN) == false) {
+        pr_err("GPIO IN %d is invalid\n", GPIO_25_IN);
+        goto cleanup_validgpio25;
+    }
+    pr_info("GPIO IN %d is valid\n", GPIO_25_IN);
 
+    if (gpio_request(GPIO_25_IN, "GPIO_25") < 0) {
+        pr_err("GPIO IN %d request failed\n", GPIO_25_IN);
+        goto cleanup_reqgpio25;
+    }
+    pr_info("GPIO IN %d requested successfully\n", GPIO_25_IN);
+
+    gpio_direction_input(GPIO_25_IN);
+
+#ifndef EN_DEBOUNCE
+    if (gpio_set_debounce(GPIO_25_IN, 200) < 0) {
+        pr_err("Error setting GPIO IN %d debounce\n", GPIO_25_IN);
+        goto cleanup_gpiosetdebounce;
+    }
+    pr_info("Successfully set GPIO IN %d debounce\n", GPIO_25_IN);
+#endif
+    // gpio_export(GPIO_21, false);
+    // pr_info("GPIO_21 export is successful\n");
+
+    GPIO_irqNum = gpio_to_irq(GPIO_25_IN);
+    pr_info("GPIO_25_IN got IRQ %d\n", GPIO_irqNum);
+
+    if (request_irq(GPIO_irqNum,
+                    (void *)gpio_irq_handler,
+                    IRQF_TRIGGER_FALLING,
+                    "gpio_dev",
+                    NULL)) {
+        pr_err("IRQ %d request for GPIO IN %d failed\n", GPIO_irqNum, GPIO_25_IN);
+        goto cleanup_gpioreqirq;
+    }
     pr_info("Module installed successfully\n");
     return EXIT_SUCCESS;
 
-cleanup_gpiorequest:
-    gpio_free(GPIO_21);
-cleanup_gpioisvalid:
+cleanup_gpioreqirq:
+#ifndef EN_DEBOUNCE
+cleanup_gpiosetdebounce:
+#endif
+cleanup_reqgpio25:
+    gpio_free(GPIO_25_IN);
+cleanup_validgpio25:
+cleanup_reqgpio21:
+    gpio_free(GPIO_21_OUT);
+cleanup_validgpio21:
     device_destroy(cls_sha, dev_num);
 cleanup_device_create_failure:
     class_destroy(cls_sha);
@@ -158,17 +231,21 @@ cleanup_cdev_add:
 
 static void __exit dexit(void)
 {
-    gpio_free(GPIO_21);
+    free_irq(GPIO_irqNum, NULL);
+    gpio_free(GPIO_25_IN); 
+    gpio_free(GPIO_21_OUT); 
     device_destroy(cls_sha, dev_num);
     class_destroy(cls_sha);
     cdev_del(&mcdev);
     unregister_chrdev_region(dev_num, REQ_DEV_CNT);
-    pr_info("Successfully:\n\
-            Gpio is free\n\
-            destroyed device\n\
-            destroyed class\n\
-            deleted cdev\n\
-            deallocated chrdev region\n");
+    pr_info("Successfully:\n"
+            "IRQ free is done\n"
+            "GPIO_25_IN free is done\n"
+            "GPIO_21_OUT free is done\n"
+            "destroyed device\n"
+            "destroyed class\n"
+            "deleted cdev\n"
+            "deallocated chrdev region\n");
     pr_info("%s: dexit\n", KBUILD_MODNAME);
 }
 
@@ -177,5 +254,5 @@ module_exit(dexit);
 
 MODULE_AUTHOR("sha");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("basic linux driver module");
-MODULE_VERSION("1:0.5" );
+MODULE_DESCRIPTION("GPIO device driver module catching vibrations to turn on/off led");
+MODULE_VERSION("1:0.36" );
---
+++ kcodes/docsldd/_build/html/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+.. docsforldd documentation master file, created by
+   sphinx-quickstart on Sun Mar 19 18:43:49 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+DOCS FOR  [ LDD ]  SOURCE FILE's:
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: GPIO IRQ's
+
+   36.GPIO.IRQ/gpio.irq
+
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
